43c9970fbe51dc5ef983b3bfe1004edb37d71c72
carlo <carlo@openforge.sh>
2025-10-06 15:58:39 +0000

    more fixes and favicon

--- frontend/src/services/http.js
+++ frontend/src/services/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { getToken } from './token'
 
-export const API_BASE_URL = 'http://localhost:8080/api'
+export const API_BASE_URL = process.env.VUE_APP_API_URL || 'http://localhost:8080/api'
 
 const http = axios.create({
   baseURL: API_BASE_URL,
